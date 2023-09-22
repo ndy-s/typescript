34e14a55da56d1ca06832e891c1629d6812d978c
Hendy <hendy.sg02@gmail.com>
2023-09-22 23:57:17 +0700

    first commit

---
+++ 0. Intro/intro.js
@@ -0,0 +1,6 @@
+var user = {
+    name: "Hendy",
+    age: 10,
+};
+console.log("Hendy");
+console.log(user.name);
---
+++ 0. Intro/intro.ts
@@ -0,0 +1,7 @@
+const user = {
+    name: "Hendy",
+    age: 10,
+};
+
+console.log("Hendy");
+console.log(user.name);
